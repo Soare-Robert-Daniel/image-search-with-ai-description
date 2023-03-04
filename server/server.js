3b9702648854c776d0aca6d041f548a7319caf4c
VisanTudorCosmin <tudor_cosmin98@yahoo.com>
2023-03-04 14:38:12 +0200

    create search endpoint

--- server/server.js
+++ server/server.js
@@ -1,25 +1,64 @@
 // Require the framework and instantiate it
+const redis = require('redis')
 const fastify = require('fastify')({ logger: true })
 const path = require('path')
 const data_examples = require('./mock-data')
 
+
+const client = redis.createClient();
+client.on('error', err => console.log('Redis Client Error', err));
 
 fastify.register(require('@fastify/static'), {
   root: path.join(__dirname, 'assets'),
   prefix: '/images/',
 })
 
-// fastify.register(require('@fastify/redis'), { 
-//   host: 'localhost', // Momentan rulez pe local (instanta de docker spearata), dar pe viitor poate fi pus pe docker compose.
-//   port: 6379, // Redis port
-//   family: 4   // 4 (IPv4) or 6 (IPv6)
-// })
+fastify.get('/search', async (request, reply) => {
+  const { redis } = fastify
+  const searchQuery = request.query.query;
+
+  if(searchQuery == undefined) {
+    reply.statusCode = 400;
+    reply.send({error: "query not found"});
+  }
+
+  console.log(`search query ${searchQuery}`);
+
+  const results = await client.ft.search('idx:prompt', `@prompt:{${searchQuery}}`);
+
+  reply.send(results);
+
+})
 
 fastify.get('/', async (request, reply) => {
-  return data_examples;
+  const results = await client.json.get('noderedis:images', {});
+  reply.send(results);
 })
 
-fastify.listen({ port: 9000 }, err => {
+fastify.listen({ port: 9000 }, async err => {
   if (err) throw err
+  await client.connect();
+
+  await client.flushAll('ASYNC', (err, succeeded) => {
+    console.log(`Redis cleaned: ${succeeded}`); 
+  });
+  
+  // create redis search index
+  await client.ft.create('idx:prompt', {
+    prompt: {
+      type: redis.SchemaFieldTypes.TEXT,
+      SORTABLE: true
+    } 
+  }, {
+    ON: 'HASH',
+    PREFIX: 'noderedis:images'
+  });
+
+  data_examples.forEach(async entry => {
+    console.log(`adding '${entry.name}' to database`);
+    await client.json.set('noderedis:images', '$', entry);
+  });
+  
   console.log(`server listening on ${fastify.server.address().port}`)
 })
+
